2afe61aad371f2b858c009c4e09710afe28984da
Kotaro Sugawara <kotarella1110@gmail.com>
2020-04-01 00:07:19 +0900

    fix: fixed bug of cordova serve command (#52)

--- src/webpackCompile.ts
+++ src/webpackCompile.ts
@@ -12,7 +12,7 @@ import { createArguments, getVersion } from './utils/yargsHelpers';
 
 module.exports = async (ctx: Context) => {
   const platforms = ['browser', 'android', 'ios'] as const;
-  if (!platforms.some((platform) => ctx.opts.platforms!.includes(platform))) {
+  if (!platforms.some((platform) => ctx.opts.platforms && ctx.opts.platforms.includes(platform))) {
     return;
   }
 
